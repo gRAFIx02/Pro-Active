271b22c145c1c16cb0889e02ec93a1e1ae7b96b4
Tahmid Rahman <tahmidrahman@iut-dhaka.edu>
2022-11-22 15:00:49 +0600

    trainer functions updated

--- pro-active/src/components/Client/TrainerAccount/index.jsx
+++ pro-active/src/components/Client/TrainerAccount/index.jsx
@@ -1,11 +1,39 @@
 import './index.scss'
+import axios from "axios";
+import {useNavigate } from "react-router-dom";
+
+
 
 const TrainerAccount =() =>
 {
+    const navigate = useNavigate();
+
+    
+    const logOut = async (e) => {
+        e.preventDefault();
+        try {
+          await axios({
+            method: 'post',
+            url: "http://localhost:5000/logout",
+            withCredentials: 'true',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            
+          })
+          localStorage.removeItem("logged-in-user");
+          navigate("/trainer_login");
+        } catch (error) {
+          console.log(error.response.data);     
+        }
+
+        
+      };
+
     return(
         <>
-            <div className='logout'>
-                <button className='logout_btn'>
+            <div className='logout' >
+                <button className='logout_btn' onClick={logOut}>
                     Log Out
                 </button>
             </div>
