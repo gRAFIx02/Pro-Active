157ac99cd497eafd208ae98c82c899bc8a15032d
gRAFIx <prottoyrafi@iut-dhaka.edu>
2022-11-22 18:05:51 +0600

    Added button to add plans

--- pro-active/src/components/Client/TrainerTP/index.jsx
+++ pro-active/src/components/Client/TrainerTP/index.jsx
@@ -1,9 +1,24 @@
 import './index.scss'
+import { Link } from 'react-router-dom'
+import workoutDefault from '../../../Assets/images/workout_default.png'
 
-const TrainerTP = () =>
-{
-    return(
+const TrainerTP = () => {
+    return (
         <>
+            <div className='add_plan'>
+                <button className='add_plan_btn'>+ Add Plan</button>
+            </div>
+            <h2 className='t_name_tag'>My Workout Plans</h2>
+            <div className='wp_outer_div'>
+                <div className='wp_inner_div'>
+                    <div className='imagefield'>
+                        <Link to='/user_homepage/wp'>
+                            <img src={workoutDefault} alt='' className='explore_images' />
+                        </Link>
+                    </div>
+                    <div className='textfield'>Default text</div>
+                </div>
+            </div>
         </>
     )
 }
