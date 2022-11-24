e514297cac60976410b04942bd1ef8df84ba30bf
gRAFIx <prottoyrafi@iut-dhaka.edu>
2022-11-24 23:00:07 +0600

    Added show exercise in user section

--- pro-active/src/components/Client/UserHomePage/index.jsx
+++ pro-active/src/components/Client/UserHomePage/index.jsx
@@ -6,6 +6,7 @@ import MyAccount from '../MyAccount'
 import TrainingPlans from '../TrainingPlans'
 import NutritionPlan from '../NutritionPlans'
 import { useEffect } from 'react'
+import ShowExercise from '../ShowExercise'
 
 const UserHomePage = () =>
 {
@@ -25,6 +26,7 @@ const UserHomePage = () =>
             <Routes>
                 <Route path='/' element={<UserHomepageLayout />}>
                     <Route index element={<Explore />} />
+                    <Route path='/exercise' element={<ShowExercise />} />
                     <Route path='/training_plan' element={<TrainingPlans />} />
                     <Route path='/nutrition_plan' element={<NutritionPlan />} />
                     <Route path='/my_ac' element={<MyAccount />} />
